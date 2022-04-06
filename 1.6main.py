db47b728d83d4f34b576e27d3784d12d9ac71489
Andrey178 <santa-andrio@mail.ru>
2022-04-06 17:22:21 +0300

    Make it in standalone functions. Wrap FileNotFoundError exception.

--- 1.6main.py
+++ 1.6main.py
@@ -1,36 +1,49 @@
 import openpyxl as openpyxl
+import requests
 import wget
 import xlrd
 
 
 def task(name):
     # Download xlsx file from source
-    # filename = wget.download('https://stepik.org/media/attachments/lesson/245266/tab.xlsx', 'tab.xlsx')
-    filename = 'tab.xlsx'
+    filename, file_url = 'tab.xlsx', 'https://stepik.org/media/attachments/lesson/245266/tab.xlsx'
+    # filename = wget.download(, 'tab.xlsx')
     print(filename)
+    file_bin = requests.get(file_url)
 
     # Open xlsx file with xlrd library and count
-    wb = xlrd.open_workbook(filename)
-    sheet_names = wb.sheet_names()
-    print(sheet_names)
-    sh = wb.sheet_by_name(sheet_names[0])
-    nmin = sh.row_values(6)[2]
-    for rownum in range(7, 27):
-        temp = sh.row_values(rownum)
-        nmin = min(nmin, temp[2])
-    print(nmin)
+    def load_xlrd(xls_filename):
+        wb = xlrd.open_workbook(file_contents=xls_filename.content)
+        sheet_names = wb.sheet_names()
+        print(sheet_names)
+        sh = wb.sheet_by_name(sheet_names[0])
+        n_min = sh.row_values(6)[2]
+        for row_num in range(7, 27):
+            temp = sh.row_values(row_num)
+            n_min = min(n_min, temp[2])
+        print(n_min)
 
     # Open xlsx file with openpyxl library and count
-    wb = openpyxl.load_workbook(filename)
-    sheet_names = wb.sheetnames
-    print(sheet_names)
-    sh = wb[sheet_names[0]]
-    nmin = sh.cell(row=7, column=3).value
-    print(f"{str(nmin)} => {str(sh.cell(row=7, column=3).coordinate)}")
-    for rownum in sh['C7':'C27']:
-        temp = rownum[0].value
-        nmin = min(nmin, temp)
-    print(nmin)
+    def load_openpyxl(xls_filename):
+        try:
+            with open(xls_filename):
+                pass
+        except FileNotFoundError:
+            xls_filename = wget.download(file_url, 'tab.xlsx')
+
+        wb = openpyxl.load_workbook(xls_filename)
+        sheet_names = wb.sheetnames
+        print(sheet_names)
+        sh = wb[sheet_names[0]]
+        n_min = sh.cell(row=7, column=3).value
+        print(f"{str(n_min)} => {str(sh.cell(row=7, column=3).coordinate)}")
+        for row_num in sh['C7':'C27']:
+            temp = row_num[0].value
+            n_min = min(n_min, temp)
+        print(n_min)
+
+    load_xlrd(file_bin)
+    load_openpyxl(filename)
 
 
 if __name__ == '__main__':
