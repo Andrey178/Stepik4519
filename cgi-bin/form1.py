21e4fa0ae2d49ed28e1df334318015e94d6c686b
Andrey178 <santa-andrio@mail.ru>
2022-04-23 17:34:34 +0300

    Find error in code of cgi-script file

--- cgi-bin/form1.py
+++ cgi-bin/form1.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 import cgi
 
+def ohash(s):
+    ans = 0
+    for c in s:
+        ans = ans * 123417 + ord(c)
+    return ans
+
 form = cgi.FieldStorage()
 text = form.getfirst("INPUT_TEXT", "не задано")
-text = text[:-1]
 
 print("Content-type: text/html\n")
 print("""<!DOCTYPE HTML>
@@ -13,8 +18,7 @@ print("""<!DOCTYPE HTML>
         </head>
         <body>""")
 
-for i in range(1, len(text) + 1, 2):
-	print("<h1>" + str(hash(text[i])) + "</h1>")
+print("<h1>" + str(ohash(text)) + "</h1>")
 
 print("""</body>
         </html>""")
